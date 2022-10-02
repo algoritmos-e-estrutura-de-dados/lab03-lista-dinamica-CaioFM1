36de0aa9acb47a0f4bce75bfcf956dee6cc84b4c
Caio F. Masumoto <caio.fornaciari@edu.unifil.br>
2022-10-02 20:58:07 -0300

    Trab

---
+++ lista.py
@@ -0,0 +1,49 @@
+from node import Node
+
+
+class Lista:
+    inicio = None
+
+    def __init__(self):
+        self.inicio = None
+
+
+    def adicionar(self, valor, inicio=False):
+        if (inicio):
+            self.adicionar_no_inicio(valor)
+        else:
+            self.adicionar_no_fim(valor)
+
+    def adicionar_no_inicio(self, valor):
+        novo = Node(valor)
+        novo.proximo = self.inicio
+        self.inicio = novo
+
+    def adicionar_no_fim(self, valor):
+        if (self.inicio == None):
+            self.inicio = Node(valor, None)
+        else:
+            aux = self.inicio
+            while (aux.proximo != None):
+                aux = aux.proximo
+
+            aux.proximo = Node(valor, None)
+
+    def remover(self, valor):
+        aux = self.inicio
+        if aux.valor == valor:
+            aux.valor = None
+            self.inicio = aux.proximo
+        while aux.proximo != None:
+            aux = aux.proximo
+            if aux.valor == valor:
+                aux.valor = None
+
+    def show(self):
+        aux = self.inicio
+        print("[", end='')
+        while (aux.proximo != None):
+            print(aux.valor, end=', ')
+            aux = aux.proximo
+        print(aux.valor)
+        print("]")
